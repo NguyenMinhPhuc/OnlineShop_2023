d262af838cf1eb04168c46b9a3d1eae35a8582b2
Nguyen Minh Phuc <nmphuc@outlook.com>
2023-05-20 20:22:47 +0700

    add them code

---
+++ Projects/WebApiWithADO/WebApiWithADO.Model/DAO/ChuDeDao.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiWithADO.Model.EF;
+
+namespace WebApiWithADO.Model.DAO
+{
+   public class ChuDeDao
+    {
+        //field
+       private Database db;
+        //Contructor -ham tao
+        public ChuDeDao()
+        {
+           db = new Database();
+        }
+
+        //Method
+        //1. Select
+        public List<ChuDe> GetChuDes()
+        {
+            string err = string.Empty;
+            DataTable dataTable = new DataTable();
+            dataTable = db.GetDataTable(ref err, "PSP_ChuDe_Select", CommandType.StoredProcedure, null);
+            List<ChuDe> chuDes = new List<ChuDe>();
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                ChuDe chuDe = new ChuDe()
+                {
+                    MaChuDe = Convert.ToInt32(dr["MaChuDe"].ToString()),
+                    TenChuDe = dr["TenChuDe"].ToString(),
+                    MoTa=dr["MoTa"].ToString(),
+                    IsDelete=Convert.ToBoolean(dr["IsDelete"].ToString())
+                };
+                chuDes.Add(chuDe);
+            }
+            return chuDes;
+        }
+
+        public ChuDe GetChuDeByID(int id)
+        {
+            string err = string.Empty;
+            DataTable dataTable = new DataTable();
+            dataTable = db.GetDataTable(ref err, "PSP_ChuDe_Select", CommandType.StoredProcedure, new System.Data.SqlClient.SqlParameter("@MaChuDe",id));
+            ChuDe chuDe = null;
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                chuDe = new ChuDe()
+                {
+                    MaChuDe = Convert.ToInt32(dr["MaChuDe"].ToString()),
+                    TenChuDe = dr["TenChuDe"].ToString(),
+                    MoTa = dr["MoTa"].ToString(),
+                    IsDelete = Convert.ToBoolean(dr["IsDelete"].ToString())
+                };
+                
+            }
+            return chuDe;
+        }
+        //2. Insert
+
+        //3. Update
+
+        //4. Delete
+
+    }
+}
---
+++ Projects/WebApiWithADO/WebApiWithADO.Model/EF/ChuDe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiWithADO.Model.EF
+{
+   public class ChuDe
+    {
+        //field
+        int maChuDe;
+        string tenChuDe, moTa;
+        bool isDelete;
+        //Ctrl + R + E
+        public int MaChuDe { get => maChuDe; set => maChuDe = value; }
+        public string TenChuDe { get => tenChuDe; set => tenChuDe = value; }
+        public string MoTa { get => moTa; set => moTa = value; }
+        public bool IsDelete { get => isDelete; set => isDelete = value; }
+        //Properties
+        //public int MaChuDe 
+        //{ 
+        //    get { return maChuDe; }
+        //    set { maChuDe = value; } 
+        //}
+        //public int MaChuDe { get; set; }
+    }
+}
